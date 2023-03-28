bdb1d751fada415e323ea99d9a494889824890f7
RynuRen <pros0327@gmail.com>
2023-03-28 16:51:43 +0900

    remove log4j2 code

--- spring/src/main/java/com/test/news/service/MemberServiceImpl.java
+++ spring/src/main/java/com/test/news/service/MemberServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
